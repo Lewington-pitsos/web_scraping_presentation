acd3e00f5b5b6271ae7659442f540629aae59d17
lewington-pitsos <lewington@student.unimelb.edu.au>
2018-03-19 23:55:31 +1100

    cleanup

--- ruby_sitting_files/load(3).php
+++
@@ -1,3 +0,0 @@
-.ts-container .ts-slide{display:none} .ts-container .ts-active{display:block} .ts-container.ts-container-inline .ts-active{display:inline}
-
-/* cache key: stevenuniverse977:resourceloader:filter:minify-css:7:3dec9af87a42bf4f7c2e2e9c47a7d8f9 */

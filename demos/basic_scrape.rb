07af078081917f055e09ea2bc9d71d3aa2978308
lewington <lewington@student.unimelb.edu.au>
2018-04-10 08:49:33 +1000

    added basic scraping demo

---
+++ demos/basic_scrape.rb
@@ -0,0 +1,13 @@
+require 'net/http'
+
+# Uniform Resource Identifier. Basicaly the equivelent of a memory address for a thing on the internet.
+
+uri = URI('https://en.wikipedia.org/wiki/Steven_Universe')
+
+p uri.scheme
+p uri.host
+p uri.path
+
+page = Net::HTTP.get(uri)
+
+p page

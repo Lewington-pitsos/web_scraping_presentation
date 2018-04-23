5c080011bc9ca328544ec9f96583c38faa48289d
lewington <lewington@student.unimelb.edu.au>
2018-04-23 18:22:50 +1000

    added commentry

--- demos/proper_scrape.rb
+++ demos/proper_scrape.rb
@@ -1,42 +1,49 @@
-# if you load a ruby file, all the constants get added to the global scope, so to set up our real time scrape we just load this file in irb
 require 'watir'
 require 'nokogiri'
 require_relative './proper_scrape_offline.rb'
-# Base Search Url, if you add a query string value it will direct us to the youtube search page for that string
 
+# if you load a ruby file, all the constants get added to the global scope, so to set up our real time scrape we just load this file in irb
+
+# Base Search Url, if you add a query string value it will direct us to the youtube search page for that string
 BSU = 'https://www.youtube.com/results?search_query='
 BR = Watir::Browser.new :firefox
 
+# offline helper classes
 QJ = QueryGenerator.new()
 AR = Archivist.new()
 
-VIDEO = '#dismissable.ytd-video-renderer' # first one is blank
 
-TITLE = '#video-title.ytd-video-renderer[title]' #id, not class
-# the url is the href of this element
-VIEWS = '#metadata-line' # the first child of this element
+# +-----------------------------------------------------------------------------+
+#                             Scrape Helper Functions
+# +-----------------------------------------------------------------------------+
 
+
+# navigates the browser to the url representing the next search query
 def next_page
   string = QJ.next_string()
   url = BSU + string
   BR.goto(url)
 end
 
+# returns an XML representation of the current page
 def xml_page
   page = BR.html
   Nokogiri::HTML(page)
 end
 
+# saves all the video data on the current page to the database
 def save_page
   page = xml_page()
   AR.save_all_videos(page)
 end
 
+# navigates to the next page and saves all the video data on it
 def single_scrape
   next_page()
   save_page()
 end
 
+# executes 'num' scrapes in sequence
 def scrape(num)
   num.times do
     single_scrape()
--- demos/proper_scrape_offline.rb
+++ demos/proper_scrape_offline.rb
@@ -2,16 +2,19 @@ require 'uri'
 require 'nokogiri'
 require 'yaml'
 
+# just (exhaustively) generates and returns unique strings made up of youtube search compatible characters
 class QueryGenerator
 
+  # basically a list of youtube-search-compatible characters (simpler than converting straight from the ascii table and having lots of conditions to weed out incompatible chars)
   @@chars = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z', '>', '<', '(', ')', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y, 'Z'
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z', '>', '<', '(', ')', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
   ]
 
   def initialize(initial_string=-1)
     self.query = [initial_string]
   end
 
+  # returns the next string
   def next_string
     increment_char()
     query_string()
@@ -21,11 +24,13 @@ class QueryGenerator
 
   attr_accessor :query
 
+  # converts the current string representation (made of numbers) to an actual string
   def query_string
     # converts each ascii number in the array into the associated ascii character, then joins the query array into a string and returns ia url safe version of it
     URI.escape(query.map { |num| @@chars[num] }.join(''))
   end
 
+  # changes the current string to the next unique string up
   def increment_char(index=0)
     # if the current character has reached the final character, we reset it to the first character and increment the next character over
     # if there is no 'next character over' we reset all the charcters to the first character, return the current index to the first character and add an additional character to the end of the array
@@ -49,6 +54,8 @@ class QueryGenerator
   end
 end
 
+
+# in charge of reading in data from an XML representation of a page and saving the data of the videos on that page to the database
 class Archivist
 
   # we first locate the video list and then find all it's children that match the video container selector (there are other elements outside the video list that match the video container selector that we want to avoid)
@@ -63,37 +70,61 @@ class Archivist
   @@decimal = '00'
   @@no_decimal = '000'
 
+  # INPUT: a Nokogiri::XML object representing the current page
+  # DOES: gets the data for all the videos on the current page and saves it all to the database
+  # OUTPUT: N/A
   def save_all_videos(xml_page)
+
+    # first we grab all the nokogiri objects representing videos on the current page
+    # then we search each for it's title, title url and view number
+    # we save each of these to an object which is added to an array
+    # after all videos have had their data added to this array we save the whole array to the database
+
     all_videos = []
-    videos = xml_page.css(VIDEO)[1.. -1]
+    videos = xml_page.css(@@video_selector)
+
     videos.each do |video|
-      title = video.css(TITLE)
-      views = video.css(VIEWS)[0].first_element_child.text.strip
-      view_number = to_integer(views)
+
+      title =         video.css(@@title_selector)
+      views =         video.css(@@views_selector)[0].first_element_child.text.strip
+      view_number =   to_integer(views)
 
       all_videos << {
-        url: title.attribute('href').value.strip,
-        views: view_number,
-        title: title.text.strip
+        url:    title.attribute('href').value.strip,
+        views:  view_number,
+        title:  title.text.strip
       }
 
     end
+
     add_to_database(all_videos)
   end
 
   private
 
+  # INPUT: a string representing the number of views for a video
+  # DOES: strips the chaff from the string and converts the number-letter numbering to pure numbers, then converts it to an int
+  # OUTPUT: the integer representing the same number
   def to_integer(string)
+    # first we strip the chaff
     string = string.gsub('views', '')
+
+    # if the string includes a decimal point we need to remove it
+    # we also need to change the conversion of letters => zeros to reflect the fact that some digits were behind the decimal point (for youtube it is only ever one)
     if string.include?('.')
       string = string.gsub('.', '')
       base = @@decimal
     else
       base = @@no_decimal
     end
+
+    # finally we convert the letter to a numeric value and return the resulting string as an integer
     string.gsub(/[KMB]/, 'K' => base, 'M' => base + '000', 'B' => base + '00000').to_i
   end
 
+  # INPUT: an array representing the data from a number of videos
+  # DOES: appends that data to the end of a yaml file already contaning a similar array
+  # OUTPUT: N/A
   def add_to_database(array)
     data = YAML::load_file('./demos/database/test.yaml') || []
     new_data = array.concat(data)
